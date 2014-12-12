4de5e2028f58d24364d4f5a5a00da306e6d38e40
Dominic Muller <nicklink483@gmail.com>
2014-12-12 06:42:19 -0800

    fixed Dare::Image to properly draw to the canvas

--- lib/dare/image.rb
+++ lib/dare/image.rb
@@ -7,18 +7,18 @@ module Dare
     # loads a new image resource
     # image = Dare::Image.new('some_image.png')
     #
-    def initialize(path = "", window = Dare.default_canvas)
+    def initialize(path = "", canvas = Dare.default_canvas)
       @img = `new Image()`
       `#{@img}.src = #{path}`
-      @window = window
+      @canvas = canvas
     end
 
     # draws image to a canvas at an x and y position
     # x and y represent the top-left corner of the image
     # image.draw(100, 200)
     #
-    def draw(x = 0, y = 0)
-      `#{@window.canvas.context}.drawImage(#{@img},#{x},#{y})`
+    def draw(x = 0, y = 0, canvas = @canvas)
+      `#{canvas.context}.drawImage(#{@img},#{x},#{y})`
     end
   end
 end
